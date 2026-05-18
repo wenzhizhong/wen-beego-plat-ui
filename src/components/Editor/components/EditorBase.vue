ebb36649a1b6833525d883d7f26c872066a856bc
qing <zhizhong_wen@163.com>
2026-05-18 14:01:41 +0800

    editor: add editor component

---
+++ src/components/Editor/components/EditorBase.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import "@wangeditor/editor/dist/css/style.css";
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+import { onBeforeUnmount, ref, shallowRef, onMounted, watch } from "vue";
+
+defineOptions({
+  name: "BaseEditor"
+});
+const props = defineProps<{
+  modelValue?: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+const mode = "default";
+// 编辑器实例，必须用 shallowRef
+const editorRef = shallowRef();
+
+// 内容 HTML
+const valueHtml = ref(props.modelValue || "");
+watch(
+  () => props.modelValue,
+  (newVal) => {
+    if (newVal !== valueHtml.value) {
+      valueHtml.value = newVal || "";
+    }
+  }
+);
+// 监听内部值变化，同步到外部
+watch(valueHtml, (newVal) => {
+  emit("update:modelValue", newVal);
+});
+
+
+// 模拟 ajax 异步获取内容
+onMounted(() => {
+});
+
+const toolbarConfig: any = { excludeKeys: "fullScreen" };
+const editorConfig = { placeholder: "请输入内容..." };
+
+const handleCreated = editor => {
+  // 记录 editor 实例，重要！
+  editorRef.value = editor;
+};
+
+// 组件销毁时，也及时销毁编辑器
+onBeforeUnmount(() => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.destroy();
+});
+</script>
+
+<template>
+  <div class="wangeditor">
+    <Toolbar
+      :editor="editorRef"
+      :defaultConfig="toolbarConfig"
+      :mode="mode"
+      style="border-bottom: 1px solid #ccc"
+    />
+    <Editor
+      v-model="valueHtml"
+      :defaultConfig="editorConfig"
+      :mode="mode"
+      style="height: 500px; overflow-y: hidden"
+      @onCreated="handleCreated"
+    />
+  </div>
+</template>
